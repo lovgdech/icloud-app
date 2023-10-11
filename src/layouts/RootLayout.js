c03d5e87a48a2f7caeeab69a66bd8009c4db156a
mrdech <nguyendanglong048@gmail.com>
2023-10-11 23:30:50 +0700

    fix form login

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -30,6 +30,7 @@ function RootLayout() {
               />
             </svg>
           </button>
+
           <p className={cx("logo")}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -10,36 +10,34 @@ function LoginPage() {
   const navigate = useNavigate();
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("video-container")}>
-        <video
-          playsinline="true"
-          autoplay="true"
-          loop="true"
-          muted="true"
-          width="100%"
+      <p className={cx("logo")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
         >
-          <source src={videoUrl} type="video/webm" />
-          <source src={videoUrl} type="video/mp4" />
-        </video>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
+          />
+        </svg>
+        <span>iCloud</span>
+      </p>
 
-        <p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
-            />
-          </svg>
-          <span>iCloud</span>
-        </p>
-      </div>
+      <video
+        playsinline="true"
+        autoplay="true"
+        loop="true"
+        muted="true"
+        width="100%"
+      >
+        <source src={videoUrl} type="video/webm" />
+        <source src={videoUrl} type="video/mp4" />
+      </video>
 
       <div className={cx("form-container")}>
         <form>
